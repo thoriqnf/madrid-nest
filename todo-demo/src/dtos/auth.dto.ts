432b212ef46562705195b60c77b7b06641534878
Thoriq Nur Faizal <thoriqnfaizal@gmail.com>
2026-04-13 16:15:11 +0700

    feat: enable global validation pipe and make name field optional in AuthDto

--- todo-demo/src/dtos/auth.dto.ts
+++ todo-demo/src/dtos/auth.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class AuthDto {
   @IsEmail()
@@ -10,6 +10,7 @@ export class AuthDto {
   @MinLength(6)
   password: string;
 
+  @IsOptional()
   @IsString()
   name?: string;
 }
