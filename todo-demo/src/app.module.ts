d6ae120c61a1fe2cd5ced52f989159ae687891cc
Thoriq Nur Faizal <thoriqnfaizal@gmail.com>
2026-04-15 13:15:55 +0700

    feat: register AppController, add public decorator to root route, and update deployment configuration

--- todo-demo/src/app.module.ts
+++ todo-demo/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AuthModule } from './modules/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './modules/users.module';
@@ -17,7 +19,9 @@ import { AtGuard } from './guards';
     UsersModule,
     TodosModule,
   ],
+  controllers: [AppController],
   providers: [
+    AppService,
     {
       provide: APP_GUARD,
       useClass: AtGuard,
--- todo-demo/src/main.ts
+++ todo-demo/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const port = process.env.PORT ?? 8000;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
   console.log(`Application listening on port ${port}`);
 }
 bootstrap();
