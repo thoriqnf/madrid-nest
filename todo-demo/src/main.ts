58d39b86a08aedecccecfc060657acdfb6fc1605
Thoriq Nur Faizal <thoriqnfaizal@gmail.com>
2026-04-15 13:02:53 +0700

    refactor: extract port variable for improved clarity in app.listen

--- todo-demo/src/main.ts
+++ todo-demo/src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 8000, '0.0.0.0');
+  const port = process.env.PORT ?? 8000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application listening on port ${port}`);
 }
 bootstrap();
