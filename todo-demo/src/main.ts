99d56e3246c62d47ec0874f992819df4d47234dd
Thoriq Nur Faizal <thoriqnfaizal@gmail.com>
2026-04-15 12:32:46 +0700

    fix: specify host for app.listen to ensure accessibility

--- todo-demo/src/main.ts
+++ todo-demo/src/main.ts
@@ -5,6 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(process.env.PORT ?? 8000, '0.0.0.0');
 }
 bootstrap();
